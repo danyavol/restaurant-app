fb8c06a587db25b5483125b4c40eefc9a30a05a3
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-05-16 02:31:18 +0300

    Added date picker

--- client/src/components/orderTable/TableList.vue
+++ client/src/components/orderTable/TableList.vue
@@ -1,18 +1,58 @@
 <script>
 import Table from './Table';
+import { DatePicker } from 'v-calendar';
 
 export default {
     components: {
-        Table
+        Table,
+        DatePicker
+    },
+    props: {
+        config: Array,
+        orders: Array
+    },
+    data() {
+        return {
+            calendar: null,
+            selectedDate: {},
+            calendarConfig: {
+                mode: "dateTime",
+                masks: { weekdays: 'WW' },
+                'min-date': new Date(),
+                'max-date': this.getMaxDate(),
+                'model-config': {
+                    timeAdjust: '00:00:00',
+                },
+                'is-range': true,
+                'is24hr': true,
+                'minute-increment': 10,
+                'is-required': true
+            },
+        }
+    },
+    mounted() {
+        this.calendar = this.$refs.calendar;
     },
     methods: {
+        getMaxDate() {
+            let now = new Date();
+            let twoWeeks = 1000 * 60 * 60 * 24 * 7 * 4;
+            return new Date(now.getTime() + twoWeeks);
+        },
         onChangeTableStatus(data) {
             console.log('order table', data);
+        }
+    },
+    watch: {
+        selectedDate() {
+            console.log('selectedDate', this.selectedDate);
         }
     }
 }
 </script>
 
 <template>
+    <DatePicker v-model="selectedDate" v-bind="calendarConfig" />
+    
     <Table :ordered="false" :number="5" :seats="6" @changeTableStatus="onChangeTableStatus" />
 </template>
--- client/src/pages/OrderTablePage.vue
+++ client/src/pages/OrderTablePage.vue
@@ -1,6 +1,7 @@
 <template>
     <p>Order table</p>
-    <TableList />
+    <TableList :config="tablesConfig" :orders="tablesOrders" />
+    
 </template>
 
 <script>
@@ -10,5 +11,34 @@ export default {
     components: {
         TableList
     },
+    data() {
+        return {
+            tablesConfig: this.getTablesConfig(),
+            tablesOrders: this.getTablesOrders()
+        }
+    },
+    methods: {
+        getTablesConfig() {
+            return [
+                { id: 1, seats: 2 },
+                { id: 2, seats: 2 },
+                { id: 3, seats: 3 },
+                { id: 4, seats: 4 },
+                { id: 5, seats: 4 },
+                { id: 6, seats: 4 },
+                { id: 7, seats: 6 },
+                { id: 8, seats: 6 },
+                { id: 9, seats: 6 },
+                { id: 10, seats: 1 },
+                { id: 11, seats: 1 },
+                { id: 12, seats: 1 }
+            ]
+        },
+        getTablesOrders () {
+            return [
+                { id: 1, tableId: 1, fromTime: 1562342234, toTime: 12323421 }
+            ]
+        }
+    }
 }
 </script>
